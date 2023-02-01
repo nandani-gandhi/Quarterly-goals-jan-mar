21a2178d53321707df7c6dd31801604de7fd00bd
nandani-gandhi <104819785+nandani-gandhi@users.noreply.github.com>
2023-02-01 11:12:03 +0530

    Update EmpUsingTreeMap.java
    iterator

--- src/Collection/EmpUsingTreeMap.java
+++ src/Collection/EmpUsingTreeMap.java
@@ -9,5 +9,11 @@ public class EmpUsingTreeMap {
         employeeTreeMap.put(1,addEmpData.employee);
         employeeTreeMap.put(2,addEmpData.employee1);
         System.out.println("Employee Details"+employeeTreeMap.entrySet());
+        Iterator iterator= employeeTreeMap.keySet().iterator();
+        while(iterator.hasNext())
+        {
+            int key = (int) iterator.next();
+            System.out.println("Key is : "+ key +" Value is : " + employeeTreeMap.get(key));
+        }
     }
 }
